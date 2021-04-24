762a063fb0947a8af985edfb6f17626740ca6e36
Blednaya Luna <alexander.shandurenko@gmail.com>
2021-04-24 17:56:55 +0400

    works? xd

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import crypto from 'crypto';
 import express from 'express';
 import bodyParser from 'body-parser';
-import { data as dataTX, broadcast } from '@waves/waves-transactions';
+import { data as dataTX, broadcast, invokeScript } from '@waves/waves-transactions';
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,6 +10,7 @@ const PORT = process.env.PORT || 8000;
 
 const nodeUrl = 'https://nodes-testnet.wavesnodes.com';
 const oracleSeed = 'six unlock sadness camera scrub above announce mixed elegant front cliff heart video practice alter';
+const assetId = '27DyPeKHyRjKH4XjVeVa9uLYvUJvLTecT51P7c38vPaG';
 
 app.get('/hello-world', (req, res) => res.send('FAQ U'));
 
@@ -28,15 +29,35 @@ app.post('/oracle/write', async (req, res) => {
 		return acc;
 	}, []);
 
-	const signerDataTX = dataTX({
+	const signedDataTX = dataTX({
 		data,
 		chainId: 'T',
 		fee: 500000,
 	}, oracleSeed);
 
-	await broadcast(signerDataTX, nodeUrl)
+	await broadcast(signedDataTX, nodeUrl)
 		.then((r) => res.send(r))
 		.catch((e) => res.send(e));
+
+	const signedTransferTX = invokeScript({
+		dApp: '3N6ANu5s8SGmgQMsSpsAvhpS3XCDfcLdvfX',
+		call: {
+			function: 'transfer',
+			args: []
+		},
+		payment: [
+			{
+				amount: 1,
+				assetId,
+			}
+		],
+		senderPublicKey: '4rc8Aajc7YNqQXTNrzVvsTiZKzZwzKWZ4JXr4N34wxTJ',
+		chainId: 'T',
+	}, 'grief february pool cherry venue bonus mule love other guard donkey live apple fall jewel');
+
+	await broadcast(signedTransferTX, nodeUrl)
+		.then((r) => console.info(r))
+		.catch((e) => console.error(e));
 })
 
 app.listen(PORT, () => {
